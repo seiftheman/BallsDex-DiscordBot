455b2b1a1c4305d09c4af10c4bdd766927343041
East Germany <156626116+eastgermany1848@users.noreply.github.com>
2025-01-23 14:23:05 +0200

    Create __init__.py

---
+++ ballsdex/packages/countryballs/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from ballsdex.packages.countryballs.cog import CountryBallsSpawner
+
+if TYPE_CHECKING:
+    from ballsdex.core.bot import BallsDexBot
+
+
+async def setup(bot: BallsDexBot):
+    cog = CountryBallsSpawner(bot)
+    await bot.add_cog(cog)
+    await cog.load_cache()
