fbf676ae632cf274f9d1c8ff68f66d42fbf0b100
East Germany <156626116+eastgermany1848@users.noreply.github.com>
2025-01-23 13:56:58 +0200

    Create __init__.py

---
+++ ballsdex/packages/config/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from ballsdex.packages.config.cog import Config
+
+if TYPE_CHECKING:
+    from ballsdex.core.bot import BallsDexBot
+
+
+async def setup(bot: BallsDexBot):
+    await bot.add_cog(Config(bot))
